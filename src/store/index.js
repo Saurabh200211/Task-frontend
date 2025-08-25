ab5c79a09b4237d05ce4bebaa76a6dbe44671a10
saurabh <satpute2002saurabh@gmail.com>
2025-08-25 21:23:51 +0530

    Initial commit - monorepo with frontend & backend

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authreducer from "./auth";
+
+const store = configureStore({
+    reducer: {
+        auth: authreducer,
+
+    },
+});
+export default store;
